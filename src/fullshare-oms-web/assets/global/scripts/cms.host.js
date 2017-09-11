ca06d9b7db59c495d52e537e064995ace87347ca
zryjj <zryjj@zryjj-PC>
2017-09-11 16:59:00 +0800

    cms.host.js文件修改url

--- src/fullshare-oms-web/assets/global/scripts/cms.host.js
+++ src/fullshare-oms-web/assets/global/scripts/cms.host.js
@@ -3,8 +3,8 @@ if (/http:\/\/t/.test(location.href)) {
         MEMBER_API_BASE = "http://tapi.fshtop.com/member-api/",
         STORE_API_BASE = "http://prevideo.fshtop.com/",
         File_Upload = "http://prevideo.fshtop.com/common/";
-        OMS_TEST  =  "http://192.168.7.208.8087/eb-manager";
-        APP_TEST  =  "http://192.168.7.208.8087/eb-manager"; 
+        OMS_TEST  =  "http://192.168.7.208:8087/eb-manager";
+        APP_TEST  =  "http://192.168.7.208:8087/eb-manager"; 
 } else if (/http:\/\/m/.test(location.href)) {
     var CMS_API_BASE = "http://api.fshtop.com/cms-manager",
         MEMBER_API_BASE = "http://api.fshtop.com/member-api/",
