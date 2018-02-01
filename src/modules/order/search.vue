55cd453f49825acf47210a4a58ed9f7ebb868f2c
zhouzone <345460126@qq.com>
2018-02-01 10:13:29 +0800

    搜索bug修复

--- src/modules/order/search.vue
+++ src/modules/order/search.vue
@@ -335,144 +335,146 @@ export default {
     this.onchange(this.searchOptions);
     this.oncreate(false, true);
     let dates = $("#createStartTimeOrder,#createEndTimeOrder");
-    dates.datepicker({
-      dateFormat: "yy-mm-dd",
-      timeFormat: "HH:mm",
-      showMonthAfterYear: true,
-      changeMonth: true,
-      changeYear: true,
-      buttonImageOnly: true,
-      stepHour: 1,
-      stepMinute: 1,
-      closeText: "确定",
-      prevText: "&#x3c;上月",
-      nextText: "下月&#x3e;",
-      currentText: "今天",
-      monthNames: [
-        "一月",
-        "二月",
-        "三月",
-        "四月",
-        "五月",
-        "六月",
-        "七月",
-        "八月",
-        "九月",
-        "十月",
-        "十一月",
-        "十二月"
-      ],
-      monthNamesShort: [
-        "一",
-        "二",
-        "三",
-        "四",
-        "五",
-        "六",
-        "七",
-        "八",
-        "九",
-        "十",
-        "十一",
-        "十二"
-      ],
-      dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
-      dayNamesShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
-      dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
-      weekHeader: "周",
-      showAnim: "highlight",
-      isClear: true, //是否显示清空
-      isRTL: false,
-      onSelect: function(selectedDate) {
-        var option = this.id == "createStartTimeOrder" ? "minDate" : "maxDate";
-        dates.not(this).datepicker("option", option, selectedDate);
-      },
-      onClose: function(data, inst) {
-        $(this).blur();
-        dates.removeAttr("disabled");
-      },
-      beforeShow: function() {
-        dates.attr("disabled", "disabled");
-        if ($("#createEndTimeOrder").datepicker("getDate") != null) {
-          return;
+    if(dates.datepicker){
+      dates.datepicker({
+        dateFormat: "yy-mm-dd",
+        timeFormat: "HH:mm",
+        showMonthAfterYear: true,
+        changeMonth: true,
+        changeYear: true,
+        buttonImageOnly: true,
+        stepHour: 1,
+        stepMinute: 1,
+        closeText: "确定",
+        prevText: "&#x3c;上月",
+        nextText: "下月&#x3e;",
+        currentText: "今天",
+        monthNames: [
+          "一月",
+          "二月",
+          "三月",
+          "四月",
+          "五月",
+          "六月",
+          "七月",
+          "八月",
+          "九月",
+          "十月",
+          "十一月",
+          "十二月"
+        ],
+        monthNamesShort: [
+          "一",
+          "二",
+          "三",
+          "四",
+          "五",
+          "六",
+          "七",
+          "八",
+          "九",
+          "十",
+          "十一",
+          "十二"
+        ],
+        dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
+        dayNamesShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+        dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+        weekHeader: "周",
+        showAnim: "highlight",
+        isClear: true, //是否显示清空
+        isRTL: false,
+        onSelect: function(selectedDate) {
+          var option = this.id == "createStartTimeOrder" ? "minDate" : "maxDate";
+          dates.not(this).datepicker("option", option, selectedDate);
+        },
+        onClose: function(data, inst) {
+          $(this).blur();
+          dates.removeAttr("disabled");
+        },
+        beforeShow: function() {
+          dates.attr("disabled", "disabled");
+          if ($("#createEndTimeOrder").datepicker("getDate") != null) {
+            return;
+          }
+          $(this).datepicker("option", "maxDate", new Date());
         }
-        $(this).datepicker("option", "maxDate", new Date());
-      }
-    });
-    dates.on("click", function() {
-      $(this).attr("disabled", "disabled");
-    });
-    dates.on("blur", this.setOptions);
+      });
+      dates.on("click", function() {
+        $(this).attr("disabled", "disabled");
+      });
+      dates.on("blur", this.setOptions);
 
-    let datess = $("#payStartTimeOrder,#payEndTimeOrder");
-    datess.datepicker({
-      dateFormat: "yy-mm-dd",
-      timeFormat: "HH:mm",
-      showMonthAfterYear: true,
-      changeMonth: true,
-      changeYear: true,
-      buttonImageOnly: true,
-      stepHour: 1,
-      stepMinute: 1,
-      closeText: "确定",
-      prevText: "&#x3c;上月",
-      nextText: "下月&#x3e;",
-      currentText: "今天",
-      monthNames: [
-        "一月",
-        "二月",
-        "三月",
-        "四月",
-        "五月",
-        "六月",
-        "七月",
-        "八月",
-        "九月",
-        "十月",
-        "十一月",
-        "十二月"
-      ],
-      monthNamesShort: [
-        "一",
-        "二",
-        "三",
-        "四",
-        "五",
-        "六",
-        "七",
-        "八",
-        "九",
-        "十",
-        "十一",
-        "十二"
-      ],
-      dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
-      dayNamesShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
-      dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
-      weekHeader: "周",
-      showAnim: "highlight",
-      isClear: true, //是否显示清空
-      isRTL: false,
-      onSelect: function(selectedDate) {
-        var option = this.id == "payStartTimeOrder" ? "minDate" : "maxDate";
-        datess.not(this).datepicker("option", option, selectedDate);
-      },
-      onClose: function(data, inst) {
-        $(this).blur();
-        datess.removeAttr("disabled");
-      },
-      beforeShow: function() {
-        datess.attr("disabled", "disabled");
-        if ($("#payEndTimeOrder").datepicker("getDate") != null) {
-          return;
+      let datess = $("#payStartTimeOrder,#payEndTimeOrder");
+      datess.datepicker({
+        dateFormat: "yy-mm-dd",
+        timeFormat: "HH:mm",
+        showMonthAfterYear: true,
+        changeMonth: true,
+        changeYear: true,
+        buttonImageOnly: true,
+        stepHour: 1,
+        stepMinute: 1,
+        closeText: "确定",
+        prevText: "&#x3c;上月",
+        nextText: "下月&#x3e;",
+        currentText: "今天",
+        monthNames: [
+          "一月",
+          "二月",
+          "三月",
+          "四月",
+          "五月",
+          "六月",
+          "七月",
+          "八月",
+          "九月",
+          "十月",
+          "十一月",
+          "十二月"
+        ],
+        monthNamesShort: [
+          "一",
+          "二",
+          "三",
+          "四",
+          "五",
+          "六",
+          "七",
+          "八",
+          "九",
+          "十",
+          "十一",
+          "十二"
+        ],
+        dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
+        dayNamesShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+        dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+        weekHeader: "周",
+        showAnim: "highlight",
+        isClear: true, //是否显示清空
+        isRTL: false,
+        onSelect: function(selectedDate) {
+          var option = this.id == "payStartTimeOrder" ? "minDate" : "maxDate";
+          datess.not(this).datepicker("option", option, selectedDate);
+        },
+        onClose: function(data, inst) {
+          $(this).blur();
+          datess.removeAttr("disabled");
+        },
+        beforeShow: function() {
+          datess.attr("disabled", "disabled");
+          if ($("#payEndTimeOrder").datepicker("getDate") != null) {
+            return;
+          }
+          $(this).datepicker("option", "maxDate", new Date());
         }
-        $(this).datepicker("option", "maxDate", new Date());
-      }
-    });
-    datess.on("click", function() {
-      $(this).attr("disabled", "disabled");
-    });
-    datess.on("blur", this.setOptions);
+      });
+      datess.on("click", function() {
+        $(this).attr("disabled", "disabled");
+      });
+      datess.on("blur", this.setOptions);
+    }
   }
 };
 </script>
